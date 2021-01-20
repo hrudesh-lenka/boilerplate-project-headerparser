24d7047c0277e71b8e6545375b6033df9f0cc51b
hrudesh-lenka <hrudesh.lenka@gmail.com>
2021-01-21 02:12:48 +0530

    added route

--- server.js
+++ server.js
@@ -21,13 +21,23 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const software = req.header('user-agent')
+  const language = req.header('accept-language')
+  const ipaddress = req.header('x-forwarded-for')
+  res.json({software,
+    language,
+    ipaddress
+  });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port http://localhost:3000');
 });
